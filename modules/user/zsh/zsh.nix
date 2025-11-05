03da9653f05e860bd2d0c81863d7e02ff71233de
vitorbborges <vitorbborges31@gmail.com>
2025-11-05 22:57:52 +0100

    fix typo

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -5,7 +5,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
     oh-my-zsh = {
@@ -24,62 +24,69 @@
       ls = "eza --icons --git --group-directories-first -l -T -L=1";
       cat = "bat";
       mamba = "micromamba";
+      man = "tldr"
+        };
+
+      history = {
+        size = 10000;
+        ignoreAllDups = true;
+        path = "$HOME/.zsh_history";
+      };
+
+      plugins = [
+
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+
+        {
+          name = "fzf-tab";
+          src = pkgs.fetchFromGitHub {
+            owner = "Aloxaf";
+            repo = "fzf-tab";
+            rev = "c2b4aa5ad2532cca91f23908ac7f00efb7ff09c9";
+            sha256 = "1b4pksrc573aklk71dn2zikiymsvq19bgvamrdffpf7azpq6kxl2";
+          };
+        }
+
+      ];
+
     };
 
-    history = {
-      size = 10000;
-      ignoreAllDups = true;
-      path = "$HOME/.zsh_history";
-    };
-
-    plugins = [
-
+    programs.fzf =
       {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
+        enable = true;
+        enableZshIntegration = true;
+      };
 
-      {
-        name = "fzf-tab";
-        src = pkgs.fetchFromGitHub {
-          owner = "Aloxaf";
-          repo = "fzf-tab";
-          rev = "c2b4aa5ad2532cca91f23908ac7f00efb7ff09c9";
-          sha256 = "1b4pksrc573aklk71dn2zikiymsvq19bgvamrdffpf7azpq6kxl2";
-        };
-      }
-
-    ];
-
-  };
-
-  programs.fzf =
-    {
+    programs.eza = {
       enable = true;
       enableZshIntegration = true;
+      extraOptions = [
+        "--group-directories-first"
+      ];
+      git = true;
+      icons = "always";
+      theme = "dracula";
     };
 
-  programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
-    extraOptions = [
-      "--group-directories-first"
+    services.tldr-update = {
+      enable = true;
+    };
+
+    home.packages = with pkgs; [
+      eza
+      bat
+      fzf
+      tldr
+      tldr-update
     ];
-    git = true;
-    icons = "always";
-    theme = "dracula";
-  };
 
-  home.packages = with pkgs; [
-    eza
-    bat
-    fzf
-  ];
-
-}
+  }
