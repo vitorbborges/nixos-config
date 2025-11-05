e9a4d32a9e8dbfe6993093436999db997265eb79
vitorbborges <vitorbborges31@gmail.com>
2025-11-06 00:40:34 +0100

    corrected hash

--- modules/user/yazi/yazi.nix
+++ modules/user/yazi/yazi.nix
@@ -11,7 +11,7 @@
           owner = "yazi-rs";
           repo = "flavors";
           rev = "main";
-          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          sha256 = "sha256-bavHcmeGZ49nNeM+0DSdKvxZDPVm3e6eaNmfmwfCid0=";
         } + "/dracula.yazi";
     };
 
