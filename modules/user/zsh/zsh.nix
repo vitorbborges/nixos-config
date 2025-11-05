1030fdd354788cf1dd45c9384f0c68a2641623bc
vitorbborges <vitorbborges31@gmail.com>
2025-11-05 19:47:57 +0100

    enable auto-suggestions

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -5,6 +5,8 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
 
     oh-my-zsh = {
       enable = true;
