56a9c4f77f3f1e7a1afb6274006934dfbf7e0591
vitorbborges <vitorbborges31@gmail.com>
2025-11-06 00:43:17 +0100

    configuring icons

--- modules/user/yazi/yazi.nix
+++ modules/user/yazi/yazi.nix
@@ -15,5 +15,36 @@
         } + "/dracula.yazi";
     };
 
+    theme.icon = {
+      dirs = [
+        { name = ".config"; text = ""; }
+        { name = ".git"; text = ""; }
+        { name = ".github"; text = ""; }
+        { name = ".npm"; text = ""; }
+        { name = "Desktop"; text = ""; }
+        { name = "Development"; text = ""; }
+        { name = "Documents"; text = ""; }
+        { name = "Downloads"; text = ""; }
+        { name = "Library"; text = ""; }
+        { name = "Movies"; text = ""; }
+        { name = "Music"; text = ""; }
+        { name = "Pictures"; text = ""; }
+        { name = "Public"; text = ""; }
+        { name = "Videos"; text = ""; }
+        { name = "nixos"; text = ""; }
+        { name = "Archive"; text = ""; }
+        { name = "Media"; text = ""; }
+        { name = "Podcasts"; text = ""; }
+        { name = "Drive"; text = ""; }
+        { name = "KP"; text = ""; }
+        { name = "Books"; text = ""; }
+        { name = "Games"; text = ""; }
+        { name = "Game Saves"; text = ""; }
+        { name = "Templates"; text = ""; }
+        { name = "Notes"; text = ""; }
+        { name = "Projects"; text = ""; }
+        { name = "Screenshots"; text = ""; }
+      ];
+    };
   };
 }
