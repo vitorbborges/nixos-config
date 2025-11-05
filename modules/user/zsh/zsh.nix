d53d53b8f441bcc421baccb5d38302bcfc1597f1
vitorbborges <vitorbborges31@gmail.com>
2025-11-05 22:58:43 +0100

    forgot a ;

--- modules/user/zsh/zsh.nix
+++ modules/user/zsh/zsh.nix
@@ -24,69 +24,69 @@
       ls = "eza --icons --git --group-directories-first -l -T -L=1";
       cat = "bat";
       mamba = "micromamba";
-      man = "tldr"
-        };
-
-      history = {
-        size = 10000;
-        ignoreAllDups = true;
-        path = "$HOME/.zsh_history";
-      };
-
-      plugins = [
-
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.8.0";
-            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-          };
-        }
-
-        {
-          name = "fzf-tab";
-          src = pkgs.fetchFromGitHub {
-            owner = "Aloxaf";
-            repo = "fzf-tab";
-            rev = "c2b4aa5ad2532cca91f23908ac7f00efb7ff09c9";
-            sha256 = "1b4pksrc573aklk71dn2zikiymsvq19bgvamrdffpf7azpq6kxl2";
-          };
-        }
-
-      ];
-
+      man = "tldr";
     };
 
-    programs.fzf =
+    history = {
+      size = 10000;
+      ignoreAllDups = true;
+      path = "$HOME/.zsh_history";
+    };
+
+    plugins = [
+
       {
-        enable = true;
-        enableZshIntegration = true;
-      };
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
 
-    programs.eza = {
-      enable = true;
-      enableZshIntegration = true;
-      extraOptions = [
-        "--group-directories-first"
-      ];
-      git = true;
-      icons = "always";
-      theme = "dracula";
-    };
+      {
+        name = "fzf-tab";
+        src = pkgs.fetchFromGitHub {
+          owner = "Aloxaf";
+          repo = "fzf-tab";
+          rev = "c2b4aa5ad2532cca91f23908ac7f00efb7ff09c9";
+          sha256 = "1b4pksrc573aklk71dn2zikiymsvq19bgvamrdffpf7azpq6kxl2";
+        };
+      }
 
-    services.tldr-update = {
-      enable = true;
-    };
-
-    home.packages = with pkgs; [
-      eza
-      bat
-      fzf
-      tldr
-      tldr-update
     ];
 
-  }
+  };
+
+  programs.fzf =
+    {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    extraOptions = [
+      "--group-directories-first"
+    ];
+    git = true;
+    icons = "always";
+    theme = "dracula";
+  };
+
+  services.tldr-update = {
+    enable = true;
+  };
+
+  home.packages = with pkgs; [
+    eza
+    bat
+    fzf
+    tldr
+    tldr-update
+  ];
+
+}
