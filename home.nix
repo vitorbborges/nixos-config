5d31107ed8028395cc44031ee17b23484dc5be6b
vitorbborges <vitorbborges31@gmail.com>
2025-11-28 15:48:37 +0100

    fix fonts

--- home.nix
+++ home.nix
@@ -40,7 +40,7 @@
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ fontPkg ]; })
+    (pkgs.nerd-fonts.override { fonts = [ fontPkg ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
