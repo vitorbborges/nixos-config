fddb303cc06215900e046bbf9b9e344f3aac932d
vitorbborges <vitorbborges31@gmail.com>
2025-11-28 18:59:26 +0100

    fix spicetify

--- modules/user/spicetify/spicetify.nix
+++ modules/user/spicetify/spicetify.nix
@@ -1,6 +1,10 @@
 { pkgs, config, lib, inputs, ... }:
 
 {
+  imports = [
+    inputs.spicetify-nix.homeManagerModules.default
+  ];
+
   programs.spicetify =
     let
       spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
