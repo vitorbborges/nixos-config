995aaebb9431966d04a6961ae9ef304f3df78ac7
vitorbborges <vitorbborges31@gmail.com>
2025-11-28 20:57:04 +0100

    python and vscode

---
+++ modules/user/development-tools/vscodium.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.userSettings.vscodium;
+in {
+  options = {
+    userSettings.vscodium = {
+      enable = lib.mkEnableOption "Enable vscodium";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = true; # Required for some extensions like Data Wrangler
+
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      profiles.default.extensions = with pkgs.vscode-extensions; [
+          ms-toolsai-datawrangler
+          github.copilot-chat
+          ms-toolsai.jupyter
+          ms-python.python
+          pkief.material-icon-theme
+          gruntfuggly.todo-tree
+          redhat.vscode-yaml
+      ];
+      profiles.default.userSettings = {
+        "keyboard.dispatch" = "keyCode";
+      };
+      mutableExtensionsDir = false;
+
+    };
+    stylix.targets.vscode.enable = true;
+    };
+}
