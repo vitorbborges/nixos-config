22fb648725d7cd0195bdbe90de41ad6ce709e7b0
vitorbborges <vitorbborges31@gmail.com>
2025-12-16 11:14:19 -0300

    fix for awatcher

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -1,3 +1,29 @@
 final: prev: {
-  awatcher = (import ../system/awatcher-package.nix) { pkgs = prev; lib = prev.lib; };
+  awatcher = prev.rustPlatform.buildRustPackage rec {
+    pname = "awatcher";
+    version = "unstable-2023-12-16";
+
+    src = prev.fetchFromGitHub {
+      owner = "2e3s";
+      repo = "awatcher";
+      rev = "312781b0460c5c678a1a4570075d9e5a1b32d294";
+      sha256 = prev.lib.fakeSha256;
+    };
+
+    cargoSha256 = prev.lib.fakeHash;
+
+    buildInputs = with prev; [
+      pkg-config
+      openssl
+      dbus
+    ];
+
+    meta = with prev.lib; {
+      description = "Window activity and idle watcher for ActivityWatch";
+      homepage = "https://github.com/2e3s/awatcher";
+      license = licenses.mpl20;
+      platforms = platforms.linux;
+      maintainers = [ ];
+    };
+  };
 }
